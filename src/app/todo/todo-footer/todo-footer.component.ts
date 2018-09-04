b545e4767f1072a8095ed13723a6ed182c3b7b3a
Pedro Pérez <e-pdperez@neoris.com>
2018-09-04 11:36:24 -0500

    Primer commit

---
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as fromFiltro from '../../filter/filter.actions';
+import * as fromTodo from '../../todo/todo.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import { Todo } from '../model/todo.model';
+import { LimpiarCompletadosTodoAction } from '../todo.actions';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styles: []
+})
+export class TodoFooterComponent implements OnInit {
+
+  pendientes: number;
+  filtrosValidos: fromFiltro.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+  filtroActual: fromFiltro.filtrosValidos;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.store.subscribe((state) => {
+      this.contarPendiente(state.todos);
+      this.filtroActual = state.filtro;
+    });
+  }
+
+  cambiarFiltro(nuevoFiltro: fromFiltro.filtrosValidos) {
+    const accion = new fromFiltro.SetFiltroAction(nuevoFiltro);
+    this.store.dispatch(accion);
+  }
+
+  contarPendiente(todos: Todo[]) {
+    this.pendientes = todos.filter((todo) => !todo.completado).length;
+  }
+
+  limpiarCompletados() {
+    const accion = new fromTodo.LimpiarCompletadosTodoAction();
+    this.store.dispatch(accion);
+  }
+}
